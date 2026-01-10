63e0db010d6fb4985cc2cfaf0243edd3d06d13d8
Daiji-gs <gs.06.yamanaka@gmail.com>
2026-01-11 00:06:44 +0900

    更新

--- funcs.php
+++ funcs.php
@@ -4,31 +4,31 @@ function h($str){
     return htmlspecialchars($str, ENT_QUOTES);
 }
 
-// //DB接続関数：db_conn()
-// function db_conn(){
-//     try {
-//         $db_name = "gsyamanaka_php02";    //データベース名
-//         $db_id   = "gsyamanaka_php02";      //アカウント名
-//         $db_pw   = "gsyamanaka_php02";          //パスワード：XAMPPはパスワード無し or MAMPはパスワード”root”に修正してください。
-//         $db_host = "mysql3112.db.sakura.ne.jp"; //DBホスト
-//         return new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host, $db_id, $db_pw);
-//     } catch (PDOException $e) {
-//         exit('DB Connection Error:'.$e->getMessage());
-//     }
-// }
-
 //DB接続関数：db_conn()
 function db_conn(){
     try {
-        $db_name = "gs_db_books";    //データベース名
-        $db_id   = "root";      //アカウント名
-        $db_pw   = "";          //パスワード：XAMPPはパスワード無し or MAMPはパスワード”root”に修正してください。
-        $db_host = "localhost"; //DBホスト
+        $db_name = "gsyamanaka_php02";    //データベース名
+        $db_id   = "gsyamanaka_php02";      //アカウント名
+        $db_pw   = "gsyamanaka_php02";          //パスワード：XAMPPはパスワード無し or MAMPはパスワード”root”に修正してください。
+        $db_host = "mysql3112.db.sakura.ne.jp"; //DBホスト
         return new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host, $db_id, $db_pw);
     } catch (PDOException $e) {
         exit('DB Connection Error:'.$e->getMessage());
     }
 }
+
+// //DB接続関数：db_conn()
+// function db_conn(){
+//     try {
+//         $db_name = "gs_db_books";    //データベース名
+//         $db_id   = "root";      //アカウント名
+//         $db_pw   = "";          //パスワード：XAMPPはパスワード無し or MAMPはパスワード”root”に修正してください。
+//         $db_host = "localhost"; //DBホスト
+//         return new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host, $db_id, $db_pw);
+//     } catch (PDOException $e) {
+//         exit('DB Connection Error:'.$e->getMessage());
+//     }
+// }
 
 
 //SQLエラー関数：sql_error($stmt)
--- insert.php
+++ insert.php
@@ -3,9 +3,14 @@
 ini_set("display_errors", 1); // 1=表示、0=非表示
 
 //1. POSTデータ取得
-$name    = $_POST["name"];
-$url     = $_POST["url"];
-$comment = $_POST["comment"];
+$name    = isset($_POST["name"]) ? $_POST["name"] : "";
+$url     = isset($_POST["url"]) ? $_POST["url"] : "";
+$comment = isset($_POST["comment"]) ? $_POST["comment"] : "";
+
+// データが空の場合はエラー
+if(empty($name) || empty($url) || empty($comment)){
+  exit("エラー：すべてのフィールドに入力してください");
+}
 
 
 //2. DB接続します
@@ -14,7 +19,7 @@ $pdo = db_conn(); //returnから救い出している
 
 
 //３．データ登録SQL作成(***INSERT INTO gs_books_table***)
-$sql="INSERT INTO gs_books_table(name,url,comment,date)VALUES(:name, :url, :comment, sysdate());"; //ここで箱を作っている。
+$sql="INSERT INTO gs_books_table(name,url,comment,date)VALUES(:name, :url, :comment, NOW());"; //ここで箱を作っている。
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':name',    $name,    PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)　ここで上記の箱に安全に情報を入れる
 $stmt->bindValue(':url',     $url,     PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
